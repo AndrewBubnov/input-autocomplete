3945b63373f3f41e04f57464e45822175b980941
Max Bubnov <m.bubnov05@gmail.com>
2023-07-28 13:44:14 +0200

    Add mock fetch delay

--- src/utils/getMockHints.ts
+++ src/utils/getMockHints.ts
@@ -1,13 +1,18 @@
+const FETCH_DELAY = 200;
+
 const generateRandomChar = () => String.fromCharCode(Math.random() * 25 + 97);
 
 export const getMockHints = (head: string) =>
-	new Promise<string[]>(resolve =>
-		resolve([
-			...new Set(
-				Array.from({ length: 15 }, () => {
-					const tail = Array.from({ length: 10 }, generateRandomChar).join('');
-					return `${head}${tail}`;
-				})
-			),
-		])
+	new Promise(resolve => setTimeout(resolve, FETCH_DELAY)).then(
+		() =>
+			new Promise<string[]>(resolve =>
+				resolve([
+					...new Set(
+						Array.from({ length: 15 }, () => {
+							const tail = Array.from({ length: 10 }, generateRandomChar).join('');
+							return `${head}${tail}`;
+						})
+					),
+				])
+			)
 	);
